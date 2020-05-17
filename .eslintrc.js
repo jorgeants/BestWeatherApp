809244071e251ecc9bb3b9c2ad7116fb8671254f
Jorge Antônio da Silva <jorgeants@gmail.com>
2020-05-17 02:35:30 -0300

    Change configurations for eslint, prittier and babel-plugin-root-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,24 +2,36 @@ module.exports = {
   env: {
     es6: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
+  extends: ['airbnb', 'prettier', 'prettier/react'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 11,
+    ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react', 'prettier'],
   rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'warn',
+      {
+        extensions: ['.jsx', '.js'],
+      },
+    ],
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        'rootPathPrefix': '~',
+        'rootPathSuffix': 'src',
+      },
+    },
   },
 };
