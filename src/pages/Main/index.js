4ce5005eb5512aa60a02f0c1efd8d9272cd7fe52
Jorge Antônio da Silva <jorgeants@gmail.com>
2020-05-18 13:52:17 -0300

    Configure get user location

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,5 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 import { useDispatch, useSelector } from 'react-redux';
+import Geolocation from '@react-native-community/geolocation';
 
 import { requestForecast } from '~/store/modules/forecast/actions';
 
@@ -17,12 +19,44 @@ import {
 } from './styles';
 
 const Main = () => {
+  const [location, setLocation] = useState(null);
+  const [watchID, setWatchID] = useState(null);
+  // const [locationLoading, setLocationLoading] = useState(false);
   const dispatch = useDispatch();
   const { loading, data } = useSelector((state) => state.forecast);
 
+  const getLocation = () => {
+    Geolocation.getCurrentPosition(
+      (position) => {
+        // const initialPosition = JSON.stringify(position);
+        setLocation(position);
+      },
+      (error) => Alert.alert('Error', JSON.stringify(error)),
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 }
+    );
+    setWatchID(
+      Geolocation.watchPosition((position) => {
+        // const lastPosition = JSON.stringify(position);
+        setLocation(position);
+      })
+    );
+  };
+
   useEffect(() => {
-    dispatch(requestForecast(-22.5435364, -44.1055362));
+    getLocation();
+    return () => {
+      Geolocation.clearWatch(watchID);
+    };
   }, []);
+
+  useEffect(() => {
+    if (location) {
+      console.log('location', location);
+      dispatch(
+        requestForecast(location.coords.latitude, location.coords.longitude)
+      );
+    }
+  }, [location]);
 
   return (
     <Background colors={[theme.primary, theme.secondary]}>
---
+++ src/store/modules/location/constants.js
@@ -0,0 +1,3 @@
+export const REQUEST_LOCATION = '@location/REQUEST_LOCATION';
+export const LOCATION_SUCCESS = '@location/LOCATION_SUCCESS';
+export const LOCATION_FAILURE = '@location/LOCATION_FAILURE';
